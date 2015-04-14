7742b4bf01e666638954fcd8534df5a5d70b0bce
pedrotfig <pedrotf76@gmail.com>
2015-04-14 12:13:27 -0300

    aaa

---
+++ RepFacil/RoomEditingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RoomEditingViewController.swift
+//  RepFacil
+//
+//  Created by Pedro Figueiredo on 4/14/15.
+//  Copyright (c) 2015 Pocket Bomb. All rights reserved.
+//
+
+import UIKit
+
+class RoomEditingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
