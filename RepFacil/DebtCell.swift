8de36250e17674b5fa8c7ca08abcf8bc4c22277d
pedrotfig <pedrotf76@gmail.com>
2015-04-15 16:43:13 -0300

    Little improvements

---
+++ RepFacil/DebtCell.swift
@@ -0,0 +1,26 @@
+//
+//  DebtCell.swift
+//  RepFacil
+//
+//  Created by Pedro Figueiredo on 4/15/15.
+//  Copyright (c) 2015 Pocket Bomb. All rights reserved.
+//
+
+import UIKit
+
+class DebtCell: UITableViewCell {
+    
+    var correspondingPerson : Person? = nil
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
